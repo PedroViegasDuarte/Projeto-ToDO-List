a0bf923586842880772e88ce48131ee3ed7b5339
Vinicius Armando Menezes de Andrade <163352551+vini-andra@users.noreply.github.com>
2025-09-03 18:20:42 -0300

    adicionando as variaveis a serem utilizadas no projeto

--- src/main/java/ucb/aplicativo/model/tarefas.java
+++ src/main/java/ucb/aplicativo/model/tarefas.java
@@ -1,4 +1,12 @@
 package ucb.aplicativo.model;
+import java.time.LocalDateTime;
 
 public class tarefas {
+
+    private long ID = 0;
+    private String titulo = "";
+    private String descricao = "";
+    private LocalDateTime dataHora = null;
+    private boolean concluido = false;
+
 }
