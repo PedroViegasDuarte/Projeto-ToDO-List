08caa50d3761fa29591fc0de1b66b1f634e3c80c
Pedro Viegas Duarte <ppviegasduarte@gmail.com>
2025-09-05 09:13:20 -0300

    Update Tarefas.java
    construtores Taregas

--- src/main/java/ucb/aplicativo/model/Tarefas.java
+++ src/main/java/ucb/aplicativo/model/Tarefas.java
@@ -8,9 +8,32 @@ public class Tarefas {
     private long ID = 0;
     private String titulo = "";
     private String descricao = "";
-    private LocalDateTime dataHora = null;
-    private boolean concluido = false;
+    private LocalDateTime dataAgora = null;
+    private boolean completa = false;
 
 
+    public Tarefas(){
+        this.dataAgora = LocalDateTime.now();
+        this.completa = false;
+    }
+
+
+    public  Tarefas(String titulo){
+        this();
+        this.titulo = titulo;
+    }
+
+    public Tarefas(String titulo, String descricao) {
+        this(titulo);
+        this.descricao = descricao;
+    }
+
+    //construtor completo
+
+    public Tarefas(Long ID, String titulo, String descricao, boolean completa) {
+        this(titulo, descricao);
+        this.ID = ID;
+        this.completa = completa;
+    }
 
 }
