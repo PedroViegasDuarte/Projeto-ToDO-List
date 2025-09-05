99501c0eb4c2128e5f864f181ed51e298efdf4bc
Thiago Renó <161513773+ThiagoReno00@users.noreply.github.com>
2025-09-05 10:02:52 -0300

    TarefaServico
    Criado o método de criação de tarefas.

--- src/main/java/ucb/aplicativo/control/TarefaServico.java
+++ src/main/java/ucb/aplicativo/control/TarefaServico.java
@@ -1,7 +1,21 @@
 package ucb.aplicativo.control;
 
+import ucb.aplicativo.model.Tarefas;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TarefaServico {
+    private List<Tarefas> tarefas = new ArrayList<>();
+    private Long contadorId = 1L;
+
+    public Tarefas criarTarefa(String titulo, String descricao) {
+
+        Tarefas tarefa = new Tarefas(contadorId++, titulo, descricao, false);
+        tarefas.add(tarefa);
+        return tarefa;
+    }
+public List<Tarefas> listaTarefas() {
+        return tarefas;
+}
 
 }
